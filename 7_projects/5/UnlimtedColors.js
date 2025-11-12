a7fbc71da8489c2a90f8b818e53a0af9465081c4
Nihalxyztr <tripathinihal2006@gmail.com>
2025-11-13 00:48:54 +0530

    2 Projects Set Time out and Interval

---
+++ 7_projects/5/UnlimtedColors.js
@@ -0,0 +1,29 @@
+//genrate random color
+
+const randomcolor=function(){
+    const hex="0123456789ABCDEF"
+    let color='#'
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+let intervalId;
+const startchangingcolor=function(){
+   if(!intervalId){
+    intervalId = setInterval(changebackgroundcolor,1000);
+   }
+
+ function changebackgroundcolor(){
+    document.body.style.backgroundColor=randomcolor()};
+  
+    
+}
+
+const stopchangingcolor=function(){
+    clearInterval(intervalId);
+    intervalId=null;
+}
+
+document.querySelector("#start").addEventListener('click',startchangingcolor)
+document.querySelector("#stop").addEventListener('click',stopchangingcolor)
